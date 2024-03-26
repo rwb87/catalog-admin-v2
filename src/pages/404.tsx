e7d3980b80d5c0ad1ae221013d51f7513a4ca90a
Mercy H <mobiledevcoach@gmail.com>
2024-03-26 20:55:22 +0530

    feat: 404 error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,38 @@
-import { Button, Flex, Heading } from "@chakra-ui/react";
+import AppLayout from "@/layouts/app.layout";
+import { Box, Button, Flex, Heading } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 export default function FourOhFour() {
     const navigate = useNavigate();
 
     return (
-        <Flex
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            gap={4}
-            minHeight="100vh"
-        >
-            <Heading>404</Heading>
+        <AppLayout activePage="">
+            <Flex
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                gap={4}
+                minHeight="100vh"
+            >
+                <img
+                    src="/images/404.webp"
+                    alt="404"
+                    width={500}
+                    height='auto'
+                    loading="lazy"
+                />
 
-            <Heading>Page not found!</Heading>
+                <Heading mt={4} className="font-secondary">Error 404</Heading>
+                <Heading fontSize='md' className="font-secondary">Looks like you have followed a broken link.</Heading>
 
-            <Button
-                size='lg'
-                className="font-bold"
-                onClick={() => navigate('/login')}
-            >Home</Button>
-        </Flex>
+                <Box mt={6}>
+                    <Button
+                        size='lg'
+                        className="font-bold"
+                        onClick={() => navigate('/looks')}
+                    >Home</Button>
+                </Box>
+            </Flex>
+        </AppLayout>
     )
 }
