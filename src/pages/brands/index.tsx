15b60c870cedbb6097fa5fdd6c26de3b84ac10d9
Mercy H <mobiledevcoach@gmail.com>
2024-04-23 14:01:01 +0530

    fix: brand create and modify issue fix

--- src/pages/brands/index.tsx
+++ src/pages/brands/index.tsx
@@ -12,8 +12,6 @@ import { IconCamera, IconChevronDown, IconEdit, IconLoader2, IconPlus, IconSearc
 import { useEffect, useMemo, useRef, useState } from "react";
 
 const BrandsView = () => {
-    const brandImageRef = useRef<any>(null);
-
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [data, setData] = useState<any>([]);
     const [filteredData, setFilteredData] = useState<any>([]);
@@ -23,7 +21,6 @@ const BrandsView = () => {
     const [editingData, setEditingData] = useState<any>({});
     const [deletingData, setDeletingData] = useState<any>({});
     const [isDeleting, setIsDeleting] = useState<boolean>(false);
-    const [isProcessing, setIsProcessing] = useState<boolean>(false);
 
     useAuthGuard('auth');
 
@@ -74,46 +71,6 @@ const BrandsView = () => {
         } catch (error: any) {
             return [];
         }
-    }
-
-    const handleUpdateData = async () => {
-        setIsProcessing(true);
-
-        const payload = new FormData();
-
-        payload.append('name', editingData?.name);
-        payload.append('pageLink', editingData?.pageLink);
-        payload.append('partnership', editingData?.partnership);
-
-        if (brandImageRef?.current.files[0]) payload.append('picture', brandImageRef?.current.files[0]);
-
-        try {
-            const response = await fetch({
-                endpoint: `/brands${editingData?.isNew ? '' : `/${editingData?.id}`}`,
-                method: editingData?.isNew ? 'POST' : 'PUT',
-                data: payload,
-                hasFiles: true,
-            });
-
-            if (response) {
-                notify('Brand saved successfully', 3000);
-
-                if(editingData?.isNew) setData([editingData, ...data]);
-                else {
-                    const index = data.findIndex((u: any) => u.id === editingData.id);
-                    const newData = [...data];
-                    newData[index] = editingData;
-                    setData(newData);
-                }
-
-                setEditingData({});
-            } else notify('An error occurred', 3000);
-        } catch (error: any) {
-            const message = error?.response?.data?.message || error?.message;
-            notify(message, 3000);
-        }
-
-        setIsProcessing(false);
     }
 
     const handleDelete = async () => {
@@ -214,6 +171,7 @@ const BrandsView = () => {
                             name: '',
                             pageLink: '',
                             pictureURL: '',
+                            partnership: 'NONE',
                             isNew: true,
                         })}
                     />
@@ -329,6 +287,7 @@ const BrandsView = () => {
                                 name: '',
                                 pageLink: '',
                                 pictureURL: '',
+                                partnership: 'NONE',
                                 isNew: true,
                             })}
                         />
@@ -345,99 +304,13 @@ const BrandsView = () => {
             />
 
             {/* Update Brand */}
-            <CustomDrawer
-                isOpen={!!editingData?.id}
-                title={editingData?.isNew ? `Create Brand` : 'Update Brand'}
-                isProcessing={isProcessing}
-                onSubmit={handleUpdateData}
-                onClose={() => setEditingData({})}
-            >
-                <Grid
-                    mt={4}
-                    templateColumns='1fr'
-                    gap={4}
-                >
-                    <FormControl id="name">
-                        <FormLabel>Brand Name</FormLabel>
-                        <Input
-                            type="text"
-                            required
-                            autoComplete="off"
-                            value={editingData?.name}
-                            onChange={(e) => setEditingData({ ...editingData, name: e.target.value })}
-                        />
-                    </FormControl>
-
-                    <FormControl id="pageLink">
-                        <FormLabel>Brand Website</FormLabel>
-                        <Input
-                            type="text"
-                            autoComplete="off"
-                            value={editingData?.pageLink}
-                            onChange={(e) => setEditingData({ ...editingData, pageLink: e.target?.value?.toLowerCase() })}
-                        />
-                    </FormControl>
-
-                    <FormControl id="partnership">
-                        <FormLabel>Brand Partnership</FormLabel>
-                        <Select
-                            value={editingData?.partnership}
-                            onChange={(e) => setEditingData({ ...editingData, partnership: e.target.value })}
-                        >
-                            <option value="NONE">None</option>
-                            <option value="GAAN">GAAN</option>
-                        </Select>
-                    </FormControl>
-
-                    <FormControl mt={4} id="creatorBanner">
-                        <FormLabel>Brand Logo (Leave blank to keep current image)</FormLabel>
-
-                        <Box position='relative'>
-                            <Image
-                                src={editingData?.pictureURL}
-                                alt={editingData?.name}
-                                width='full'
-                                loading="lazy"
-                                aspectRatio={21 / 9}
-                                rounded='lg'
-                                objectFit='contain'
-                                bgColor='gray.100'
-                                onError={(e: any) => {
-                                    e.target.src = '/images/cover-placeholder.webp';
-                                    e.target.onerror = null;
-                                }}
-                            />
-
-                            {/* Edit Cover Button */}
-                            <IconButton
-                                position='absolute'
-                                top='2'
-                                right='2'
-                                rounded='full'
-                                colorScheme='gray'
-                                aria-label='Edit cover photo'
-                                icon={<IconCamera size={22} />}
-                                onClick={() => brandImageRef.current.click()}
-                            />
-                        </Box>
-                    </FormControl>
-
-                    {/* Brand Logo input */}
-                    <input
-                        type="file"
-                        accept="image/*"
-                        ref={brandImageRef}
-                        hidden
-                        onChange={(e: any) => {
-                            const photo = e.target.files[0];
-
-                            if(!photo) return;
-
-                            setEditingData({ ...editingData, pictureURL: URL.createObjectURL(photo) });
-                        }}
-                    />
-                </Grid>
-            </CustomDrawer>
+            <UpdateBrandDrawer
+                brand={editingData}
+                onSave={async () => {
+                    setIsLoading(true);
+                    await getData();
+                }}
+            />
 
             {/* Delete Dialog */}
             <Confirmation
@@ -648,6 +521,150 @@ const TableRow = ({ item, onEdit, onDelete }: TableRowProps) => {
                     : null
             }
         </>
+    )
+}
+
+type UpdateBrandDrawerProps = {
+    brand: any,
+    onSave: (data: any) => void,
+}
+const UpdateBrandDrawer = ({ brand, onSave }: UpdateBrandDrawerProps) => {
+    const brandImageRef = useRef<any>(null);
+    const [editingData, setEditingData] = useState<any>({});
+    const [isProcessing, setIsProcessing] = useState<boolean>(false);
+
+    useEffect(() => {
+        setEditingData(JSON.parse(JSON.stringify(brand)));
+    }, [brand]);
+
+    const handleUpdateData = async () => {
+        setIsProcessing(true);
+
+        const payload = new FormData();
+
+        payload.append('name', editingData?.name);
+        payload.append('pageLink', editingData?.pageLink);
+        payload.append('partnership', editingData?.partnership);
+
+        if (brandImageRef?.current.files[0]) payload.append('picture', brandImageRef?.current.files[0]);
+
+        try {
+            const response = await fetch({
+                endpoint: `/brands${editingData?.isNew ? '' : `/${editingData?.id}`}`,
+                method: editingData?.isNew ? 'POST' : 'PUT',
+                data: payload,
+                hasFiles: true,
+            });
+
+            if (response) {
+                notify('Brand saved successfully', 3000);
+
+                onSave?.(editingData?.isNew ? { ...response, isNew: true } : editingData);
+
+                setEditingData({});
+            } else notify('An error occurred', 3000);
+        } catch (error: any) {
+            const message = error?.response?.data?.message || error?.message;
+            notify(message, 3000);
+        }
+
+        setIsProcessing(false);
+    }
+
+    return (
+        <CustomDrawer
+            isOpen={!!editingData?.id}
+            title={editingData?.isNew ? `Create Brand` : 'Update Brand'}
+            isProcessing={isProcessing}
+            onSubmit={handleUpdateData}
+            onClose={() => setEditingData({})}
+        >
+            <Grid
+                mt={4}
+                templateColumns='1fr'
+                gap={4}
+            >
+                <FormControl id="name">
+                    <FormLabel>Brand Name</FormLabel>
+                    <Input
+                        type="text"
+                        required
+                        autoComplete="off"
+                        value={editingData?.name}
+                        onChange={(e) => setEditingData({ ...editingData, name: e.target.value })}
+                    />
+                </FormControl>
+
+                <FormControl id="pageLink">
+                    <FormLabel>Brand Website</FormLabel>
+                    <Input
+                        type="text"
+                        autoComplete="off"
+                        value={editingData?.pageLink}
+                        onChange={(e) => setEditingData({ ...editingData, pageLink: e.target?.value?.toLowerCase() })}
+                    />
+                </FormControl>
+
+                <FormControl id="partnership">
+                    <FormLabel>Brand Partnership</FormLabel>
+                    <Select
+                        value={editingData?.partnership}
+                        onChange={(e) => setEditingData({ ...editingData, partnership: e.target.value })}
+                    >
+                        <option value="NONE">None</option>
+                        <option value="GAAN">GAAN</option>
+                    </Select>
+                </FormControl>
+
+                <FormControl mt={4} id="creatorBanner">
+                    <FormLabel>Brand Logo (Leave blank to keep current image)</FormLabel>
+
+                    <Box position='relative'>
+                        <Image
+                            src={editingData?.pictureURL || '/images/cover-placeholder.webp'}
+                            alt={editingData?.name}
+                            width='full'
+                            loading="lazy"
+                            aspectRatio={21 / 9}
+                            rounded='lg'
+                            objectFit='contain'
+                            bgColor='gray.100'
+                            onError={(e: any) => {
+                                e.target.src = '/images/cover-placeholder.webp';
+                                e.target.onerror = null;
+                            }}
+                        />
+
+                        {/* Edit Cover Button */}
+                        <IconButton
+                            position='absolute'
+                            top='2'
+                            right='2'
+                            rounded='full'
+                            colorScheme='gray'
+                            aria-label='Edit cover photo'
+                            icon={<IconCamera size={22} />}
+                            onClick={() => brandImageRef.current.click()}
+                        />
+                    </Box>
+                </FormControl>
+
+                {/* Brand Logo input */}
+                <input
+                    type="file"
+                    accept="image/*"
+                    ref={brandImageRef}
+                    hidden
+                    onChange={(e: any) => {
+                        const photo = e.target.files[0];
+
+                        if(!photo) return;
+
+                        setEditingData({ ...editingData, pictureURL: URL.createObjectURL(photo) });
+                    }}
+                />
+            </Grid>
+        </CustomDrawer>
     )
 }
 
